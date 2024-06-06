2c7d3202ebf130bf0b243d75490f57184fb8bc47
raonikeshnk <raonky222@gmail.com>
2024-06-06 19:35:23 +0530

    update06 june

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import Main from "./components/Main";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  return ( 
-    <>
-    <Main/>
-    </>
-   );
+  return (
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Main />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,65 +1,67 @@
 import "./css/Header.css";
+import { Link } from "react-router-dom";
+
 function Header() {
     return (
-        <nav class="container navbar navbar-expand-lg navbar-light bg-light p-0 rounded-pill">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#"><img class="dct-logo" src="images/dct logo.png" width="77px" alt="Digital Computer Technology Logo"/></a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+        <nav className="container navbar navbar-expand-lg navbar-light bg-light p-0 rounded-pill">
+        <div className="container-fluid">
+          <Link className="navbar-brand" to="#"><img className="dct-logo" src="images/dct logo.png" width="77px" alt="Digital Computer Technology Logo"/></Link>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
           </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Home</a>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <Link className="nav-link active" aria-current="page" to="#">Home</Link>
               </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">About Us</a>
+              <li className="nav-item">
+                <Link className="nav-link" to="#">About Us</Link>
               </li>
-              <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              <li className="nav-item dropdown">
+                <Link className="nav-link dropdown-toggle" to="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                   Courses
-                </a>
-                <ul class="dropdown-menu mt-4 border-0 bg-transparent" aria-labelledby="navbarDropdown">
-                  <li><a class="dropdown-item" href="#"><img src="images/rscit 1.png" alt=""/>RS-CIT</a></li>
-                  <li><a class="dropdown-item" href="#"><img src="images/tally-marks 1.png" alt=""/>TALLY WITH GST</a></li>
-                  <li><a class="dropdown-item" href="#"><img src="images/design 1.png" alt=""/>WEB DESIGNER</a></li>
-                  <li><a class="dropdown-item" href="#"><img src="images/hacking 1.png" alt=""/>ETHICAL HACKING</a></li>
-                  <li><a class="dropdown-item" href="#"><img src="images/eng 1.png" alt=""/>ENGLISH SPOKEN</a></li>
-                  <li><a class="dropdown-item" href="#"><img src="images/programming 1.png" alt=""/>CODING FOR KID'S</a></li>
-                  <li><a class="dropdown-item" href="#"><img src="images/coding 1.png" alt=""/>WEB DEVELOPMENT</a></li>
-                  <li><a class="dropdown-item" href="#"><img src="images/developer 1.png" alt=""/>APP DEVELOPMENT</a></li>
-                  <li><a class="dropdown-item" href="#"><img src="images/digital-marketing.png" alt=""/>DIGITAL MARKETING</a></li>
-                  <li><a class="dropdown-item" href="#"><img src="images/game-development 1.png" alt=""/>GAME DEVELOPMENT</a></li>
+                </Link>
+                <ul className="dropdown-menu mt-4 border-0 bg-transparent" aria-labelledby="navbarDropdown">
+                  <li><Link className="dropdown-item" to="#"><img src="images/rscit 1.png" alt=""/>RS-CIT</Link></li>
+                  <li><Link className="dropdown-item" to="#"><img src="images/tally-marks 1.png" alt=""/>TALLY WITH GST</Link></li>
+                  <li><Link className="dropdown-item" to="#"><img src="images/design 1.png" alt=""/>WEB DESIGNER</Link></li>
+                  <li><Link className="dropdown-item" to="#"><img src="images/hacking 1.png" alt=""/>ETHICAL HACKING</Link></li>
+                  <li><Link className="dropdown-item" to="#"><img src="images/eng 1.png" alt=""/>ENGLISH SPOKEN</Link></li>
+                  <li><Link className="dropdown-item" to="#"><img src="images/programming 1.png" alt=""/>CODING FOR KID'S</Link></li>
+                  <li><Link className="dropdown-item" to="#"><img src="images/coding 1.png" alt=""/>WEB DEVELOPMENT</Link></li>
+                  <li><Link className="dropdown-item" to="#"><img src="images/developer 1.png" alt=""/>APP DEVELOPMENT</Link></li>
+                  <li><Link className="dropdown-item" to="#"><img src="images/digital-marketing.png" alt=""/>DIGITAL MARKETING</Link></li>
+                  <li><Link className="dropdown-item" to="#"><img src="images/game-development 1.png" alt=""/>GAME DEVELOPMENT</Link></li>
                 </ul>
               </li>
-              <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              <li className="nav-item dropdown">
+                <Link className="nav-link dropdown-toggle" to="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                   Services
-                </a>
-                <ul class="dropdown-menu bg-transparent border-0 mt-4" aria-labelledby="navbarDropdown">
-                <li><a class="dropdown-item" href="#"><img src="images/google ad.png" alt=""/>GOOGLE AD</a></li>
-                  <li><a class="dropdown-item" href="#"><img src="images/facebook-ad.png" alt=""/>FACEBOOK AD</a></li>
-                  <li><a class="dropdown-item" href="#"><img src="images/design 1.png" alt=""/>WEB DESIGN</a></li>
-                  <li><a class="dropdown-item" href="#"><img src="images/ui-ux-design.png" alt=""/>UI/UX DESIGN</a></li>
-                  <li><a class="dropdown-item" href="#"><img src="images/cybersecurity.png" alt=""/>CYBERSECURITY</a></li>
-                  <li><a class="dropdown-item" href="#"><img src="images/programming 1.png" alt=""/>WEB SOLUTION</a></li>
-                  <li><a class="dropdown-item" href="#"><img src="images/remote-it.png" alt=""/>REMOTE IT SUPPORT</a></li>
-                  <li><a class="dropdown-item" href="#"><img src="images/developer 1.png" alt=""/>APP DEVELOPMENT</a></li>
-                  <li><a class="dropdown-item" href="#"><img src="images/digital-marketing.png" alt=""/>DIGITAL MARKETING</a></li>
-                  <li><a class="dropdown-item" href="#"><img src="images/responshiv.png" alt=""/>RESPONSHIV DESIGN</a></li>
-                  <li><a class="dropdown-item" href="#"><img src="images/ecommerce 1.png" alt=""/>E-COMMERCE PLATFORMS</a></li>
-                  <li><a class="dropdown-item" href="#"><img src="images/workshop 1.png" alt=""/>TRAINING AND WORKSHOPS</a></li>
-                  <li><a class="dropdown-item" href="#"><img src="images/social-media 1.png" alt=""/>SOCIAL MEDIA MANAGEMENT</a></li>
-                  <li><a class="dropdown-item" href="#"><img src="images/content-strategy 1.png" alt=""/>CMS</a></li>
-                  <li><a class="dropdown-item" href="#"><img src="images/seo (1) 1.png" alt=""/>SEO</a></li>
+                </Link>
+                <ul className="dropdown-menu bg-transparent border-0 mt-4" aria-labelledby="navbarDropdown">
+                <li><Link className="dropdown-item" to="#"><img src="images/google ad.png" alt=""/>GOOGLE AD</Link></li>
+                  <li><Link className="dropdown-item" to="#"><img src="images/facebook-ad.png" alt=""/>FACEBOOK AD</Link></li>
+                  <li><Link className="dropdown-item" to="#"><img src="images/design 1.png" alt=""/>WEB DESIGN</Link></li>
+                  <li><Link className="dropdown-item" to="#"><img src="images/ui-ux-design.png" alt=""/>UI/UX DESIGN</Link></li>
+                  <li><Link className="dropdown-item" to="#"><img src="images/cybersecurity.png" alt=""/>CYBERSECURITY</Link></li>
+                  <li><Link className="dropdown-item" to="#"><img src="images/programming 1.png" alt=""/>WEB SOLUTION</Link></li>
+                  <li><Link className="dropdown-item" to="#"><img src="images/remote-it.png" alt=""/>REMOTE IT SUPPORT</Link></li>
+                  <li><Link className="dropdown-item" to="#"><img src="images/developer 1.png" alt=""/>APP DEVELOPMENT</Link></li>
+                  <li><Link className="dropdown-item" to="#"><img src="images/digital-marketing.png" alt=""/>DIGITAL MARKETING</Link></li>
+                  <li><Link className="dropdown-item" to="#"><img src="images/responshiv.png" alt=""/>RESPONSHIV DESIGN</Link></li>
+                  <li><Link className="dropdown-item" to="#"><img src="images/ecommerce 1.png" alt=""/>E-COMMERCE PLATFORMS</Link></li>
+                  <li><Link className="dropdown-item" to="#"><img src="images/workshop 1.png" alt=""/>TRAINING AND WORKSHOPS</Link></li>
+                  <li><Link className="dropdown-item" to="#"><img src="images/social-media 1.png" alt=""/>SOCIAL MEDIA MANAGEMENT</Link></li>
+                  <li><Link className="dropdown-item" to="#"><img src="images/content-strategy 1.png" alt=""/>CMS</Link></li>
+                  <li><Link className="dropdown-item" to="#"><img src="images/seo (1) 1.png" alt=""/>SEO</Link></li>
                 </ul>
               </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Contact Us</a>
+              <li className="nav-item">
+                <Link className="nav-link" to="#">Contact Us</Link>
               </li>
             </ul>
-            <form class="d-flex">
-              <button class="btn btn-outline-success"><a class="text-decoration-none" href="">ENQUIRY FORM</a></button>
+            <form className="d-flex">
+              <button className="btn btn-outline-success"><Link className="text-decoration-none" to="">ENQUIRY FORM</Link></button>
             </form>
           </div>
         </div>
